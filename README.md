5b4675c382518f418da1b78b61e71bc28a5b082a
zeta30 <102247888+zeta30@users.noreply.github.com>
2022-05-15 19:02:13 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+### Heroku 
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/ arzugavargasf@gmail.com/zeta12)
---
+++ conf.py
@@ -0,0 +1,4 @@
+api_hash = "213e85670cd03dfdcfc4936c86d153a2"
+api_id = 15091118
+bot_token = "5222714351:AAGJIy9EWsfa6HEZtsDL9IU7HxeCaGYqm1o"
+bot_admin_group = -1001766577678
